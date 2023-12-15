9519bfefa255c1ae39e5c17c4ade539326eb3153
邓杰 <1171276417@qq.com>
2023-12-16 04:01:03 +0800

    feat: 实现图片浏览器缩放图片，实现全屏浏览长图

---
+++ TopBuzz_demo/Content/DetailPage/Controller/DJDetailViewController.m
@@ -0,0 +1,89 @@
+//
+//  DJDetailViewController.m
+//  TopBuzz_demo
+//
+//  Created by 邓杰 on 2023/12/14.
+//
+
+#import "DJDetailViewController.h"
+#import "DJDetailView.h"
+#import "DJScreen.h"
+
+@interface DJDetailViewController () <DJNavigationViewDelegate, DJDetailViewDelegate, UIScrollViewDelegate>
+@property (nonatomic, strong) DJCollectionItemInfo *itemInfo;
+@property (nonatomic, strong) DJDetailView *detailView;
+@property (nonatomic, strong) UIScrollView *fullPictureBrowser;
+@property (nonatomic, strong) UIScrollView *currentSrollView;
+@property (nonatomic, strong) UIImageView *currentImageView;
+
+@end
+
+@implementation DJDetailViewController
+
+- (instancetype)initWithItemInfo:(DJCollectionItemInfo *)itemInfo {
+    self = [self init];
+    if (self) {
+        _itemInfo = itemInfo;
+        DJDetailNavigationBar *navBar = [[DJDetailNavigationBar alloc] initWithFrame:CGRectMake(0, TOPSTATUSBAR_HEIGHT, SCREEN_WIDTH, NAVIGATION_HEIGHT) itemInfo:itemInfo];
+        _detailView = [[DJDetailView alloc] initWithFrame:CGRectMake(0, VIEW_Y(navBar) + VIEW_HEIGHT(navBar), SCREEN_WIDTH, SCREEN_HEIGHT) itemInfo:itemInfo];
+        
+        navBar.delegate = self;
+        _detailView.dj_delegate = self;
+
+        [self.view addSubview:navBar];
+        [self.view addSubview:_detailView];
+
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor whiteColor]];
+
+    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [_detailView loadMorePicture];
+}
+
+
+
+
+
+
+- (void)loadFullPictureBrowser:(UIScrollView *)fullPictureBrowser {
+    _fullPictureBrowser = fullPictureBrowser;
+    for (UIScrollView *subview in _fullPictureBrowser.subviews) {
+        // 判断子视图的类类型
+        if ([subview isKindOfClass:[UIScrollView class]]) {
+            subview.delegate = self;
+            subview.maximumZoomScale = 3.0;
+            subview.minimumZoomScale = 1.0;
+        }
+    }
+    [self.view addSubview:_fullPictureBrowser];
+}
+
+- (void)exitFullPictureBrowser {
+    [_fullPictureBrowser removeFromSuperview];
+}
+
+- (void)popDetailViewController { 
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    for (UIView *subview in scrollView.subviews) {
+        if ([subview isKindOfClass:[UIImageView class]]) {
+            return subview;
+        }
+    }
+    return NULL;
+}
+
+
+@end
---
+++ TopBuzz_demo/Content/DetailPage/View/DJDetailNavigationBar.m
@@ -0,0 +1,71 @@
+//
+//  DJDetailNavigationBar.m
+//  TopBuzz_demo
+//
+//  Created by 邓杰 on 2023/12/15.
+//
+
+#import "DJDetailNavigationBar.h"
+#import "DJScreen.h"
+#import "DJColor.h"
+#import "DJWebImage.h"
+
+#define BACKBTN_RADIUS 12
+#define PROFILEIMAGEVIEW_RADIUS 20
+
+@implementation DJDetailNavigationBar
+
+- (instancetype)initWithFrame:(CGRect)frame itemInfo:(DJCollectionItemInfo *)info {
+    self = [self initWithFrame:frame];
+    if (self) {
+        // 返回按钮
+        _backBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_backBtn setFrame:CGRectMake(20, VIEW_HEIGHT(self) / 2 - BACKBTN_RADIUS - 2, BACKBTN_RADIUS * 2, BACKBTN_RADIUS * 2)];
+        [_backBtn setBackgroundImage:[UIImage imageNamed:@"back"] forState:UIControlStateNormal];
+        [_backBtn setShowsTouchWhenHighlighted:YES];
+        [self addSubview:_backBtn];
+        
+        // 头像展示
+        _profileImageView = [[UIImageView alloc] init];
+        [_profileImageView setFrame:CGRectMake(VIEW_X(_backBtn) + VIEW_Y(_backBtn) + 30, VIEW_HEIGHT(self) / 2 - PROFILEIMAGEVIEW_RADIUS - 2, PROFILEIMAGEVIEW_RADIUS * 2, PROFILEIMAGEVIEW_RADIUS * 2)];
+        [_profileImageView setContentMode:UIViewContentModeScaleAspectFill];
+        [_profileImageView setClipsToBounds:YES];
+        [_profileImageView.layer setMasksToBounds:YES];
+        [_profileImageView.layer setCornerRadius:PROFILEIMAGEVIEW_RADIUS];
+        [DJWebImage djWebImageWithImageView:_profileImageView urlString:info.profileImageString];
+        [self addSubview:_profileImageView];
+        
+        // 用户名
+        _username = [[UILabel alloc] init];
+        [_username setFrame:CGRectMake(VIEW_X(_profileImageView) + VIEW_WIDTH(_profileImageView) + 15, VIEW_Y(_profileImageView), 250, PROFILEIMAGEVIEW_RADIUS + 5)];
+        [_username setText:info.username];
+        [_username setTextColor:LIGHT_ORANGE];
+        [_username setFont:[UIFont systemFontOfSize:14 weight:UIFontWeightMedium]];
+        [_username setTextAlignment:NSTextAlignmentLeft];
+        [self addSubview:_username];
+        
+        // 发布时间和发布地
+        _created_at_region = [[UILabel alloc] init];
+        [_created_at_region setFrame:CGRectMake(VIEW_X(_username), VIEW_Y(_username) + VIEW_HEIGHT(_username) - 5, 250, PROFILEIMAGEVIEW_RADIUS)];
+        [_created_at_region setText:[[info.created_at stringByAppendingString:@"    "] stringByAppendingString:info.region_name]];
+        [_created_at_region setTextColor:DARK_GRAY];
+        [_created_at_region setFont:[UIFont systemFontOfSize:11]];
+        [_created_at_region setTextAlignment:NSTextAlignmentLeft];
+        [self addSubview:_created_at_region];
+        
+        
+        
+        [_backBtn addTarget:self action:@selector(backController) forControlEvents:UIControlEventTouchUpInside];
+        
+    }
+    return self;
+}
+
+
+- (void)backController {
+    if ([self.delegate respondsToSelector:@selector(popDetailViewController)]) {
+        [self.delegate popDetailViewController];
+    }
+}
+
+@end
